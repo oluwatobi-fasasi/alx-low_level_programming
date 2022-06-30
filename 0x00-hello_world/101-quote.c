2ccb9f6b932150ed61ccfa580648dc6e9371ada8
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2022-06-30 12:12:05 -0700

    edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,6 @@
  */
 int main(void)
 {
-	char text1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, text1, 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
