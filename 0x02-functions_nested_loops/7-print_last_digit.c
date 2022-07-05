78c58b4fc2524b3fe1f47e33b9fa5edd7afe7dc9
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2022-07-05 07:44:30 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print digit
+ *
+ * @n: input number
+ *
+ * Return: Always 
+ */
+int print_last_digit(int n)
+{
+	int i;
+
+	i = n % 10;
+	if (i < 0)
+	{
+		_putchar(-i + 48);
+		return (-i);
+	}
+	else
+	{
+		_putchar(i + 48);
+		return (i);
+	}
+}
