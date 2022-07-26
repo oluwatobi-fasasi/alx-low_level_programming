e70ed2f6e13a2ec0bfe16d5da67b2a11086bfb27
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2022-07-26 00:52:58 -0700

    edited

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,9 +13,8 @@ void ch_free_grid(char **grid, unsigned int height)
 	if (grid != NULL && height != 0)
 	{
 		for (; height > 0; height--)
-		{
 			free(grid[height]);
-		}
+
 		free(grid[height]);
 		free(grid);
 	}
@@ -33,17 +32,14 @@ char **strtow(char *str)
 	unsigned int c, height, i, j, a1;
 
 	if (str == NULL || *str == '\0')
-	{
 		return (NULL);
-	}
-	for (c = height = 0; str[c] != '\0'; c++)
-	{
+
+	for (c = height = 0; str[c] != '\0'; c++){
 		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-		{
 			height++;
-		}
-	}
+
 	aout = malloc((height + 1) * sizeof(char *));
+
 	if (aout == NULL || height == 0)
 	{
 		free(aout);
@@ -54,9 +50,8 @@ char **strtow(char *str)
 		for (c = a1; str[c] != '\0'; c++)
 		{
 			if (str[c] == ' ')
-			{
 				a1++;
-			}
+
 			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
 				aout[i] = malloc((c - a1 + 2) * sizeof(char));
@@ -69,9 +64,8 @@ char **strtow(char *str)
 			}
 		}
 		for (j = 0; a1 <= c; a1++, j++)
-		{
 			aout[i][j] = str[a1];
-		}
+
 		aout[i][j] = '\0';
 	}
 	aout[i] = NULL;
