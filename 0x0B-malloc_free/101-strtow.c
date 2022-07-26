451fcdb17a66f10b04c760591e75bf22371ecbe5
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2022-07-26 01:05:58 -0700

    edited

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,7 +14,6 @@ void ch_free_grid(char **grid, unsigned int height)
 	{
 		for (; height > 0; height--)
 			free(grid[height]);
-
 		free(grid[height]);
 		free(grid);
 	}
@@ -33,13 +32,10 @@ char **strtow(char *str)
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	for (c = height = 0; str[c] != '\0'; c++){
+	for (c = height = 0; str[c] != '\0'; c++)
 		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			height++;
-
 	aout = malloc((height + 1) * sizeof(char *));
-
 	if (aout == NULL || height == 0)
 	{
 		free(aout);
@@ -51,7 +47,6 @@ char **strtow(char *str)
 		{
 			if (str[c] == ' ')
 				a1++;
-
 			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
 				aout[i] = malloc((c - a1 + 2) * sizeof(char));
@@ -65,7 +60,6 @@ char **strtow(char *str)
 		}
 		for (j = 0; a1 <= c; a1++, j++)
 			aout[i][j] = str[a1];
-
 		aout[i][j] = '\0';
 	}
 	aout[i] = NULL;
