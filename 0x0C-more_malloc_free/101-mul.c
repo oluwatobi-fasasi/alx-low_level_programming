169871f062f4380f9d174a8e87c7e43d248d74ac
oluwatobi-fasasi <wasiufasasi79@gmail.com>
2022-07-28 18:56:43 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -74,7 +74,8 @@ int main(int argc, char *argv[])
 			carry += result[len1 + len2 + 1] + (digit1 *digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
-		}if (carry > 0)
+		}
+		if (carry > 0)
 		result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
